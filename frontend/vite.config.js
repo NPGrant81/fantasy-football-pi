bdb8a897e5f77b22a84569c507c05626a652f853
NPGrant81 <117535662+NPGrant81@users.noreply.github.com>
2026-02-11 19:14:04 -1000

    Updated for testing leagues page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // This tells the frontend: "If you see a request for /leagues, send it to Python port 8000"
+      '/leagues': 'http://127.0.0.1:8000',
+      '/owners': 'http://127.0.0.1:8000',
+      '/draft-pick': 'http://127.0.0.1:8000',
+      '/advisor': 'http://127.0.0.1:8000',
+    }
+  }
 })
